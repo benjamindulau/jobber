0f209a98f6c588d8163c6463f301663a52bfda99
Benjamin Dulau <benjamin.dulau@anonymation.com>
2012-05-09 12:06:55 +0200

    Lib architecture in progress...

---
+++ src/Jobber/Adapter/AdapterInterface.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Jobber\Adapter;
+
+interface AdapterInterface
+{
+    public function getQueues();
+
+    /**
+     * Returns queue with the given name
+     *
+     * @param string $name
+     * @return \Jobber\Model\QueueInterface
+     */
+    public function getQueue($name);
+
+    /**
+     * Create a queue with name $name
+     * and returns it
+     *
+     * @param string $name
+     * @return \Jobber\Model\QueueInterface
+     */
+    public function createQueue($name);
+
+    /**
+     * Deletes queue with the given name
+     * and all its jobs
+     *
+     * @param string $name
+     * @return void
+     */
+    public function deleteQueue($name);
+}
--- src/Jobber/Jobber.php
+++ src/Jobber/Jobber.php
@@ -2,6 +2,7 @@
 
 namespace Jobber;
 
+use Jobber\Adapter\AdapterInterface;
 use Symfony\Component\EventDispatcher\EventDispatcherInterface;
 use Jobber\Model\JobInterface;
 use Jobber\JobData\JobDataInterface;
@@ -11,22 +12,24 @@ use Jobber\JobberEvents;
 class Jobber
 {
     protected $dispatcher;
+    protected $adapter;
 
-    public function __construct(EventDispatcherInterface $dispatcher)
+    public function __construct(AdapterInterface $adapter, EventDispatcherInterface $dispatcher)
     {
         $this->dispatcher = $dispatcher;
+        $this->adapter = $adapter;
     }
 
-    public function enqueue(JobInterface $job, JobDataInterface $data)
+    public function enqueue($queue, JobInterface $job, JobDataInterface $data)
     {
         $event = new JobberEvent($job, $data);
         $this->dispatcher->dispatch(JobberEvents::BEFORE_ENQUEUE, $event);
 
         // Do something
-
+        $this->adapter->getQueue($queue)->addJob($job);
+x
         $event = new JobberEvent($job, $data);
         $this->dispatcher->dispatch(JobberEvents::AFTER_ENQUEUE, $event);
     }
-
 
 }
